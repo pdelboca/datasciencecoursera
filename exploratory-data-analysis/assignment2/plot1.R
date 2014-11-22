4dbf2d3fdbdda3bef0b10faba58a5c69329170ca
pdelboca <patriciodelboca@gmail.com>
2014-11-22 19:12:24 -0300

    Excercise 2 and changes on plot 1

--- exploratory-data-analysis/assignment2/plot1.R
+++ exploratory-data-analysis/assignment2/plot1.R
@@ -1,17 +1,20 @@
 setwd("~/Repos/datasciencecoursera//exploratory-data-analysis/assignment2/")
 
+## Read the files
 scc <- readRDS("./data/Source_Classification_Code.rds")
 summary <- readRDS("./data//summarySCC_PM25.rds")
 
 ## Have total emissions from PM2.5 decreased in the United States from 1999 to 2008?
 
+# Calculate Totals
 totals <- aggregate(summary$Emissions, by = list(summary$year), FUN = sum)
 names(totals) <- c("Year", "Emissions")
 
+# Plot data
 png("plot1.png")
-plot(totals$Year,totals$Emissions, 
+barplot(totals$Emissions, 
      main = "Total PM2.5 Emission per Year",
+     names.arg = totals$Year,
      xlab = "Year",
-     ylab = "Total PM2.5 Emission",
-     pch = 20)
+     ylab = "Total PM2.5 Emission")
 dev.off()
