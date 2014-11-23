291b9dfc955d26c43ee9280c12fa8eb4e67596a9
pdelboca <patriciodelboca@gmail.com>
2014-11-22 22:05:20 -0300

    Excercise 6

---
+++ exploratory-data-analysis/assignment2/plot6.R
@@ -0,0 +1,26 @@
+setwd("~/Repos/datasciencecoursera//exploratory-data-analysis/assignment2/")
+
+## Compare emissions from motor vehicle sources in Baltimore City with emissions
+## from motor vehicle sources in Los Angeles County, California (fips == "06037").
+## Which city has seen greater changes over time in motor vehicle emissions?
+
+# Load data
+scc <- readRDS("./data/Source_Classification_Code.rds")
+summary <- readRDS("./data//summarySCC_PM25.rds")
+
+# Subset
+scc.motor.vehicle <- subset(scc, grepl("Vehicles",scc$EI.Sector))
+summary.cities <- subset(summary, fips == "24510" | fips == "06037")
+summary.cities.vehicles <- subset(summary.cities, 
+                                  summary.cities$SCC %in% scc.motor.vehicle$SCC)
+
+# Sumary
+totals <- with(summary.cities.vehicles,
+               aggregate(Emissions, by=list(fips,year), FUN = sum))
+names(totals) <- c("City","Year","Emissions")
+
+# Plot
+png("plot6.png", width = 700)
+ggplot(totals, aes(Year,Emissions)) + geom_line(aes(color=City)) +
+    labs(title = "Emissions from motor vehicle sources in Baltimore City vs Los Angeles County, California.")
+dev.off()
