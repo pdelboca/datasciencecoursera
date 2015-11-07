beb86292539691026e64dca0f23a428b3ec61969
Patricio Del Boca <patriciodelboca@gmail.com>
2015-11-07 11:48:57 -0300

    Week 1 - Practical Machine Learning

---
+++ practical-machine-learning/Quiz1.R
@@ -0,0 +1,29 @@
+# QUIZ 1
+
+# Question 1
+# Which of the following are steps in building a machine learning algorithm?
+
+# Asking the right question. 
+
+# Question 2
+# Suppose we build a prediction algorithm on a data set and it is 100% accurate 
+# on that data set. Why might the algorithm not work well if we collect a 
+# new data set?
+
+# Our algorithm may be overfitting the training data, predicting both the signal and the noise. 
+
+#Question 3
+#What are typical sizes for the training and test sets?
+
+# 60% in the training set, 40% in the testing set. 
+
+# Question 4
+# What are some common error rates for predicting binary variables (i.e. variables with two possible values like yes/no, disease/normal, clicked/didn't click)?
+
+# Accuracy 
+
+
+# Question 5
+# Suppose that we have created a machine learning algorithm that predicts whether a link will be clicked with 99% sensitivity and 99% specificity. The rate the link is clicked is 1/1000 of visits to a website. If we predict the link will be clicked on a specific visit, what is the probability it will actually be clicked?
+
+# 9%
